a55f9a8472119c64f7d13fc862b6bb9d7b4ce7b3
ShrutiSingh-git-hub <85101747+ShrutiSingh-git-hub@users.noreply.github.com>
2021-06-17 11:59:22 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -79,14 +79,14 @@
 	  }
 	
 
-	  for(int i = 0; i < s; i++)
+	  for(int i = 0; i < size; i++)
 	    count[arr[i]]++;
 	
 
 	   max = count[0];
 	
 
-	    for(int i = 1; i < size; i++)
+	    for(int i = 1; i < s; i++)
 	    {
 	        if(max < count[i])
 	            {
